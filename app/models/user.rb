d3cb1516fca723a8e39ecbb7a1b4432d967d52c0
Re-OKMT <cherryblossom.snowstorm27@gmail.com>
2021-06-19 14:43:50 +0900

    ユーザー管理機能実装

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,21 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  validates :nickname           , presence: true
+  validates :email              , presence: true
+  validates :encrypted_password , presence: true, format: { with: /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i, message: "は6文字以上かつ英数字をそれぞれ含めてください" }
+
+  with_options presence: true, format: { with: /\A[ぁ-ゔァ-ヴ\p{Ideographic}ａ-ｚＡ-Ｚ０-９]+\z/, message: 'に全角文字を使用してください' } do
+    validates :family_name 
+    validates :first_name 
+    validates :kana_family_name   , presence: true
+    validates :kana_first_name    , presence: true
+  end
+
+  validates :birthday           , presence: true
+  
+  has_many :items
+  has_many :buys
+  
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
   devise_for :users
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  root to: "furimas#index"
+
+  #resources :users, only: [:edit, :update, :show]
+   # resources :furimas, only: [:new, :create, :show, :edit, :update, :destroy ] do
+   # end
+    # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
