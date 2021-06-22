776102f0075bd5f87966fa65ff8f13fe23c7de8e
Re-OKMT <cherryblossom.snowstorm27@gmail.com>
2021-06-22 22:42:40 +0900

    単体テストコード作成

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,21 +3,22 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+has_many :items
+has_many :buys
 
-  validates :nickname           , presence: true
-  validates :email              , presence: true
-  validates :encrypted_password , presence: true, format: { with: /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i, message: "は6文字以上かつ英数字をそれぞれ含めてください" }
+  validates :nickname             , presence: true
+  #validates :email               , presence: true
+  validates :password             , presence: true, format: { with: /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i }
 
-  with_options presence: true, format: { with: /\A[ぁ-ゔァ-ヴ\p{Ideographic}ａ-ｚＡ-Ｚ０-９]+\z/, message: 'に全角文字を使用してください' } do
+  with_options presence: true, format: { with: /\A[ぁ-ゔァ-ヴ\p{Ideographic}ａ-ｚＡ-Ｚ０-９]+\z/} do
     validates :family_name 
     validates :first_name 
     validates :kana_family_name   , presence: true
     validates :kana_first_name    , presence: true
   end
 
-  validates :birthday           , presence: true
-  
-  has_many :items
-  has_many :buys
+  validates :birthday             , presence: true
+   
+
   
 end
