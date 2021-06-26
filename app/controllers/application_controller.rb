9f495e82a2e270f7c82e721a9fea2296bc9c143a
Re-OKMT <cherryblossom.snowstorm27@gmail.com>
2021-06-26 18:35:48 +0900

    Revert "Revert "Merge pull request #2 from Re-OKMT/ユーザー管理機能""
    This reverts commit 1e91dc82d2630b77d7462045916344b13ef92903.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,19 @@
 class ApplicationController < ActionController::Base
+  #before_action :configure_permitted_parameters, if: :devise_controller?  #configure_permitted_parametersを防ぐ
+  #before_action :basic_auth #Basic認証は全てのコントローラで行いたい
+  # before_action :authenticate_user!
+
+  private
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up,
+                                      keys: [:nickname, :family_name, :first_name, :kana_family_name, :kana_first_name,
+                                             :birthday])
+  end
+
+  def basic_auth
+    authenticate_or_request_with_http_basic do |username, password|
+      username == ENV['BASIC_AUTH_USER'] && password == ENV['BASIC_AUTH_PASSWORD']  # 環境変数を読み込む記述に変更
+    end
+  end
 end
