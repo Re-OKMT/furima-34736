2991a534db53ae7a2e461860f96f9e4cabd155c6
Re-OKMT <cherryblossom.snowstorm27@gmail.com>
2021-07-07 13:05:08 +0900

    商品一覧表示機能2

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,9 @@ Rails.application.routes.draw do
   devise_for :users
   root to: "items#index"
 
+  #devise_scope :user do
+  #  get '/users/sign_out' => 'devise/sessions#destroy'
+  #end
  
 
   resources :items, only: [:index, :new, :create]
